d89dcb6d33e54839f367514b380c54ed63d05f33
Rdhwnzaki <zaki85.rmz@gmail.com>
2022-10-06 16:29:59 +0700

    add tugas 2

--- tugas2.js
+++ tugas2.js
@@ -1,5 +1,5 @@
 const printGenapGanjil = 5
-for (i = 1; i <= printGenapGanjil; i++) {
+for (let i = 1; i <= printGenapGanjil; i++) {
     if (i % 2 == 0) {
         console.log(`${i} merupakan bilangan genap`)
     } else {
